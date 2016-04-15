1a962530ddefb2cb10a6081ef9884f5d1665ca6e
Hybridmax <hybridmax95@gmail.com>
2016-04-15 21:40:27 +0200

    ramdisk: Updated STweaks & Kernel Script to work properly

--- build_image/I9505/ramdisk/sbin/post-init.sh
+++ build_image/I9505/ramdisk/sbin/post-init.sh
@@ -1,6 +1,7 @@
-#!/sbin/busybox sh
+#!/system/bin/sh
 
-BB=/sbin/busybox
+BB=/system/xbin/busybox
+PROFILE_PATH=/data/.hybridmax
 
 # Mounting partition in RW mode
 
@@ -11,17 +12,28 @@ OPEN_RW()
 }
 OPEN_RW;
 
-sleep 5;
+# Installing Busybox
+$BB chmod 06755 /system/xbin/busybox
+/system/xbin/busybox --install -s /system/xbin/
 
-# Run Qualcomm scripts
-$BB sh /init.qcom.post_boot.sh;
+# Fixing ROOT
+/system/xbin/daemonsu --auto-daemon &
 
-sleep 5;
+sleep 1;
+
 OPEN_RW;
+
+# Fix init.d folder permissions
+$BB chown -R root.root /system/etc/init.d
+$BB chmod -R 755 /system/etc/init.d
+$BB chmod 755 /system/etc/init.d/*
+
+# Start script in init.d folder
+$BB run-parts /system/etc/init.d/
 
 # Symlink
 if [ ! -e /cpufreq ]; then
-	$BB ln -s /sys/devices/system/cpu/cpu0/cpufreq /cpufreq;
+	$BB ln -s /sys/devices/system/cpu/cpu0/cpufreq/ /cpufreq;
 	$BB ln -s /sys/devices/system/cpu/cpufreq/ /cpugov;
 fi;
 
@@ -30,54 +42,21 @@ $BB rm -rf /cache/lost+found/* 2> /dev/null;
 $BB rm -rf /data/lost+found/* 2> /dev/null;
 $BB rm -rf /data/tombstones/* 2> /dev/null;
 
+OPEN_RW;
+
 CRITICAL_PERM_FIX()
 {
 	# critical Permissions fix
-	$BB chown -R system:system /data/anr;
 	$BB chown -R root:root /tmp;
 	$BB chown -R root:root /res;
 	$BB chown -R root:root /sbin;
 	$BB chown -R root:root /lib;
 	$BB chmod -R 777 /tmp/;
 	$BB chmod -R 775 /res/;
-	$BB chmod -R 06755 /sbin/ext/;
-	$BB chmod -R 0777 /data/anr/;
-	$BB chmod -R 0400 /data/tombstones;
 	$BB chmod 06755 /sbin/busybox;
+	$BB chmod 06755 /system/xbin/busybox;
 }
 CRITICAL_PERM_FIX;
-
-# oom and mem perm fix
-$BB chmod 666 /sys/module/lowmemorykiller/parameters/cost;
-$BB chmod 666 /sys/module/lowmemorykiller/parameters/adj;
-$BB chmod 666 /sys/module/lowmemorykiller/parameters/minfree;
-
-# Make sure we own the device nodes
-$BB chown system /sys/devices/system/cpu/cpu0/cpufreq/*;
-$BB chown system /sys/devices/system/cpu/cpu1/online;
-$BB chown system /sys/devices/system/cpu/cpu2/online;
-$BB chown system /sys/devices/system/cpu/cpu3/online;
-$BB chmod 666 /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor;
-$BB chmod 666 /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq;
-$BB chmod 666 /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq;
-$BB chmod 444 /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_cur_freq;
-$BB chmod 444 /sys/devices/system/cpu/cpu0/cpufreq/stats/*;
-$BB chmod 666 /sys/devices/system/cpu/cpu1/online;
-$BB chmod 666 /sys/devices/system/cpu/cpu2/online;
-$BB chmod 666 /sys/devices/system/cpu/cpu3/online;
-$BB chmod 666 /sys/module/msm_thermal/parameters/*;
-$BB chmod 666 /sys/module/msm_thermal/core_control/enabled;
-$BB chmod 666 /sys/class/kgsl/kgsl-3d0/max_gpuclk;
-$BB chmod 666 /sys/devices/platform/kgsl-3d0/kgsl/kgsl-3d0/pwrscale/trustzone/governor;
-
-$BB chown -R root:root /data/property;
-$BB chmod -R 0700 /data/property;
-
-# Set ondemand GPU governor as default
-echo "ondemand" > /sys/devices/platform/kgsl-3d0/kgsl/kgsl-3d0/pwrscale/trustzone/governor;
-
-# Make sure our max gpu clock is set via sysfs
-echo "450000000" > /sys/class/kgsl/kgsl-3d0/max_gpuclk;
 
 # Prop tweaks
 setprop persist.adb.notify 0
@@ -85,17 +64,22 @@ setprop persist.service.adb.enable 1
 setprop pm.sleep_mode 1
 setprop logcat.live disable
 setprop profiler.force_disable_ulog 1
+setprop ro.ril.disable.power.collapse 0
+setprop persist.service.btui.use_aptx 1
 
-# Fixing ROOT
-/system/xbin/daemonsu --auto-daemon &
+# Make sure that max gpu clock is set by default to 450 MHz
+$BB echo 450000000 > /sys/class/kgsl/kgsl-3d0/max_gpuclk;
 
 # STweaks support
+$BB rm -f /system/app/HybridTweaks.apk > /dev/null 2>&1;
+$BB rm -f /system/app/Hulk-Kernel sTweaks.apk > /dev/null 2>&1;
+$BB rm -f /system/app/STweaks.apk > /dev/null 2>&1;
 $BB rm -f /system/app/STweaks_Googy-Max.apk > /dev/null 2>&1;
 $BB rm -f /system/app/GTweaks.apk > /dev/null 2>&1;
 $BB rm -f /data/app/com.gokhanmoral.stweaks* > /dev/null 2>&1;
 $BB rm -f /data/data/com.gokhanmoral.stweaks*/* > /dev/null 2>&1;
-$BB chown root.root /system/app/STweaks.apk;
-$BB chmod 644 /system/app/STweaks.apk;
+$BB chown root.root /system/priv-app/STweaks.apk;
+$BB chmod 644 /system/priv-app/STweaks.apk;
 
 if [ ! -d /data/.hybridmax ]; then
 	$BB mkdir -p /data/.hybridmax;
@@ -109,7 +93,7 @@ ccxmlsum=`md5sum /res/customconfig/customconfig.xml | awk '{print $1}'`
 if [ "a${ccxmlsum}" != "a`cat /data/.hybridmax/.ccxmlsum`" ];
 then
    $BB rm -f /data/.hybridmax/*.profile;
-   echo ${ccxmlsum} > /data/.hybridmax/.ccxmlsum;
+   $BB echo ${ccxmlsum} > /data/.hybridmax/.ccxmlsum;
 fi;
 
 [ ! -f /data/.hybridmax/default.profile ] && cp /res/customconfig/default.profile /data/.hybridmax/default.profile;
@@ -117,45 +101,38 @@ fi;
 [ ! -f /data/.hybridmax/balanced.profile ] && cp /res/customconfig/balanced.profile /data/.hybridmax/balanced.profile;
 [ ! -f /data/.hybridmax/performance.profile ] && cp /res/customconfig/performance.profile /data/.hybridmax/performance.profile;
 
+chmod -R 0777 /data/.hybridmax/;
+chmod 777 $PROFILE_PATH/default.profile
+
 read_defaults;
 read_config;
 
 # Android logger
 if [ "$logger_mode" == "on" ]; then
-	echo "1" > /sys/kernel/logger_mode/logger_mode;
+	$BB echo "1" > /sys/kernel/logger_mode/logger_mode;
 else
-	echo "0" > /sys/kernel/logger_mode/logger_mode;
+	$BB echo "0" > /sys/kernel/logger_mode/logger_mode;
 fi;
 
-# zRam
-if [ "$sammyzram" == "on" ];then
-UNIT="M"
-	/system/bin/rtccd3 -a "$zramdisksize$UNIT"
-	echo "0" > /proc/sys/vm/page-cluster;
-fi;
-
-# scheduler
-	echo "$int_scheduler" > /sys/block/mmcblk0/queue/scheduler;
-	echo "$int_read_ahead_kb" > /sys/block/mmcblk0/bdi/read_ahead_kb;
-	echo "$ext_scheduler" > /sys/block/mmcblk1/queue/scheduler;
-	echo "$ext_read_ahead_kb" > /sys/block/mmcblk1/bdi/read_ahead_kb;
+# Scheduler
+	$BB echo "$int_scheduler" > /sys/block/mmcblk0/queue/scheduler;
+	$BB echo "$int_read_ahead_kb" > /sys/block/mmcblk0/bdi/read_ahead_kb;
+	$BB echo "$ext_scheduler" > /sys/block/mmcblk1/queue/scheduler;
+	$BB echo "$ext_read_ahead_kb" > /sys/block/mmcblk1/bdi/read_ahead_kb;
 
 # CPU
-	echo "$scaling_governor_cpu0" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor;
-	echo "$scaling_governor_cpu0" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor;
-	echo "$scaling_governor_cpu0" > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor;
-	echo "$scaling_governor_cpu0" > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor;
-	echo "$scaling_max_freq" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq;
-	echo "$scaling_min_freq" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq;
-	echo "$scaling_max_freq" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq;
-	echo "$scaling_min_freq" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq;
-	echo "$scaling_max_freq" > /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq;
-	echo "$scaling_min_freq" > /sys/devices/system/cpu/cpu2/cpufreq/scaling_min_freq;
-	echo "$scaling_max_freq" > /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq;
-	echo "$scaling_min_freq" > /sys/devices/system/cpu/cpu3/cpufreq/scaling_min_freq;
-
-# Enable kmem interface for everyone by GM
-	echo "0" > /proc/sys/kernel/kptr_restrict;
+	$BB echo "$scaling_governor_cpu0" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor;
+	$BB echo "$scaling_governor_cpu0" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor;
+	$BB echo "$scaling_governor_cpu0" > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor;
+	$BB echo "$scaling_governor_cpu0" > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor;
+	$BB echo "$scaling_max_freq" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq;
+	$BB echo "$scaling_min_freq" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq;
+	$BB echo "$scaling_max_freq" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq;
+	$BB echo "$scaling_min_freq" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq;
+	$BB echo "$scaling_max_freq" > /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq;
+	$BB echo "$scaling_min_freq" > /sys/devices/system/cpu/cpu2/cpufreq/scaling_min_freq;
+	$BB echo "$scaling_max_freq" > /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq;
+	$BB echo "$scaling_min_freq" > /sys/devices/system/cpu/cpu3/cpufreq/scaling_min_freq;
 
 # Apply STweaks defaults
 export CONFIG_BOOTING=1
@@ -164,8 +141,18 @@ export CONFIG_BOOTING=
 
 OPEN_RW;
 
-# Fix critical perms again after init.d mess
+$BB chmod 777 $PROFILE_PATH/default.profile
+
+# Fix critical perms again
 	CRITICAL_PERM_FIX;
 	
+sleep 1;
+
 # script finish here, so let me know when
-echo "Hybridmax Kernel script correctly applied" > /data/local/tmp/hybridmax_Kernel;
+rm /data/.hybridmax/Kernel-Test.log
+touch /data/.hybridmax/Kernel-Test.log
+echo "Kernel Tuning Script succesfully applied!" > /data/.hybridmax/Kernel-Test.log;
+
+$BB mount -t rootfs -o remount,ro rootfs
+$BB mount -o remount,ro /system
+
