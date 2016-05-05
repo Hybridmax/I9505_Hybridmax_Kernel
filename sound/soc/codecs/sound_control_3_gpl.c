6e413e9db58f3bbbae400ee89f7a76971a35e5bb
robcore <robpatershuk@gmail.com>
2016-05-05 16:16:55 -0600

    Sound_Control - updated speaker reg values for fauxsound3

--- sound/soc/codecs/sound_control_3_gpl.c
+++ sound/soc/codecs/sound_control_3_gpl.c
@@ -249,7 +249,7 @@ static ssize_t speaker_gain_show(struct kobject *kobj,
 			tabla_read(fauxsound_codec_ptr,
 				TABLA_A_CDC_RX5_VOL_CTL_B2_CTL),
 			tabla_read(fauxsound_codec_ptr,
-				TABLA_A_CDC_RX6_VOL_CTL_B2_CTL));
+				TABLA_A_CDC_RX7_VOL_CTL_B2_CTL));
 
 }
 
@@ -264,7 +264,7 @@ static ssize_t speaker_gain_store(struct kobject *kobj,
 		tabla_write(fauxsound_codec_ptr,
 			TABLA_A_CDC_RX5_VOL_CTL_B2_CTL, lval);
 		tabla_write(fauxsound_codec_ptr,
-			TABLA_A_CDC_RX6_VOL_CTL_B2_CTL, rval);
+			TABLA_A_CDC_RX7_VOL_CTL_B2_CTL, rval);
 	}
 	return count;
 }
